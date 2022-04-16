6cb33269a5bbb452ed3661f5504c8da35e091fb4
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2022-04-16 15:34:18 -0700

    shortened up the constructor function

--- index.js
+++ index.js
@@ -29,20 +29,23 @@ const monster = {
 /// constructor
 
 function Character(data) {
-    this.elementId = data.elementId;
-    this.name = data.name;
-    this.avatar = data.avatar;
-    this.health = data.health;
-    this.diceCount = data.diceCount;
+    // this.elementId = data.elementId;
+    // this.name = data.name;
+    // this.avatar = data.avatar;
+    // this.health = data.health;
+    // this.diceCount = data.diceCount;
+
+    Object.assign(this, data) /// Does the same thing the 5 lines above does
 
     this.getCharacterHtml = function(){
-        const diceHtml = getDiceHtml(this.diceCount)
+        const {elementId, name, avatar, health, diceCount} = this;
+        const diceHtml = getDiceHtml(diceCount)
 
-        document.getElementById(this.elementId).innerHTML =
+        document.getElementById(elementId).innerHTML =
             `<div class="character-card">
-                <h4 class="name"> ${this.name} </h4>
-                <img class="avatar" src="${this.avatar}" />
-                <div class="health">health: <b> ${this.health} </b></div>
+                <h4 class="name"> ${name} </h4>
+                <img class="avatar" src="${avatar}" />
+                <div class="health">health: <b> ${health} </b></div>
                 <div class="dice-container">    
                     ${diceHtml}
                 </div>
