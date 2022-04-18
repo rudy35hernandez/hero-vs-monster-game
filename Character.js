62c1848ffc506e738814e696ca1c8f9888b60dbb
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2022-04-18 10:49:56 -0700

    added more methods to render score  with attack button

--- Character.js
+++ Character.js
@@ -1,35 +1,38 @@
-import {getDiceRollArray} from './utils.js'
-
+import {getDiceRollArray, getDicePlaceholderHtml} from './utils.js'
+/*
+Challenge
+1. In the getDiceHtml method, map over currentDiceScore 
+to return this string of html template for each element:
+<div class="dice">${num}</div>`. Save this new array
+to diceArray.
+2. Modify the attack() function in index.js to get our 
+app working again.
+*/
 function Character(data) {
-    // this.elementId = data.elementId;
-    // this.name = data.name;
-    // this.avatar = data.avatar;
-    // this.health = data.health;
-    // this.diceCount = data.diceCount;
-    Object.assign(this, data) /// Does the same thing the 5 lines above does
-
-    this.getDiceHtml = function(diceCount) {
-        return getDiceRollArray(diceCount).map(function(num){ 
-            return  `<div class="dice">${num}</div>`
+    Object.assign(this, data)
+    
+    this.diceArray = getDicePlaceholderHtml(this.diceCount)
+    
+    this.getDiceHtml = function() {
+        this.currentDiceScore = getDiceRollArray(this.diceCount)
+        this.diceArray = this.currentDiceScore.map(function(num){
+            return `<div class="dice">${num}</div>`
         }).join('')
-    }
-
-    this.getCharacterHtml = function(){
-        const {elementId, name, avatar, health, diceCount} = this;
-        const diceHtml = this.getDiceHtml(diceCount)
-
-        return `<div class="character-card">
+    } 
+    
+    this.getCharacterHtml = function () {
+        const { elementId, name, avatar, health, diceCount } = this;      
+        
+           return `
+            <div class="character-card">
                 <h4 class="name"> ${name} </h4>
                 <img class="avatar" src="${avatar}" />
                 <div class="health">health: <b> ${health} </b></div>
-                <div class="dice-container">    
-                    ${diceHtml}
+                <div class="dice-container">
+                    ${this.diceArray}
                 </div>
-            </div>`;
-    }
-
+            </div>`
+    }  
 }
-
-
 
 export default Character
--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 import characterData from "./data.js";
 import Character from "./Character.js";
 
-const wizard = new Character(characterData.hero)
-const orc = new Character(characterData.monster)
+
+
+document.getElementById("attack-button").addEventListener("click", attack)
+
+function attack(){
+    wizard.getDiceHtml()
+    orc.getDiceHtml()
+    render()
+}
 
 function render(){
     document.getElementById("hero").innerHTML = wizard.getCharacterHtml()
     document.getElementById("monster").innerHTML = orc.getCharacterHtml()
 }
+
+const wizard = new Character(characterData.hero)
+const orc = new Character(characterData.monster)
 
 render()
