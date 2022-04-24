3b2a13b74a42a4044d28cab49f1a1b8406de8b08
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2022-04-23 21:59:17 -0700

    added monster variable to allow us to switch monsters after wizard wins

--- index.js
+++ index.js
@@ -7,32 +7,32 @@ let monstersArray = ["orc", "demon", "goblin"];
 
 
 function getNewMonster(arr){
-    const nextMonsterData = characterData[monstersArray.shift()]
-    return nextMonsterData
+    const nextMonsterData = characterData[arr.shift()]
+    return nextMonsterData ? new Character(nextMonsterData) : {}
 }
 
 function attack(){
     wizard.getDiceHtml()
-    orc.getDiceHtml()
-    wizard.takeDamage(orc.currentDiceScore)
-    orc.takeDamage(wizard.currentDiceScore)
+    monster.getDiceHtml()
+    wizard.takeDamage(monster.currentDiceScore)
+    monster.takeDamage(wizard.currentDiceScore)
     render()
 
-    if(wizard.dead || orc.dead){
+    if(wizard.dead || monster.dead){
         endGame()
     }
 }
 
 function endGame(){
-    const endEmoji = wizard.health === 0 && orc.health === 0 ?
+    const endEmoji = wizard.health === 0 && monster.health === 0 ?
         "☠️" :
         wizard.health > 0 ? "🔮" :
         "☠️"
         
-    const endMessage = wizard.health === 0 && orc.health === 0 ?
+    const endMessage = wizard.health === 0 && monster.health === 0 ?
         "No victors - all creatures are dead" :
         wizard.health > 0 ? "The Wizard Wins" :
-        "The Orc is Victorious"
+        `The monster is Victorious`
         
     let endGameHtml = `<div class="end-game">
         <h2>Game Over</h2>
@@ -45,10 +45,10 @@ function endGame(){
 
 function render(){
     document.getElementById("hero").innerHTML = wizard.getCharacterHtml()
-    document.getElementById("monster").innerHTML = orc.getCharacterHtml()
+    document.getElementById("monster").innerHTML = monster.getCharacterHtml()
 }
 
 const wizard = new Character(characterData.hero)
-const orc = new Character(characterData.monster)
+let monster = getNewMonster(monstersArray)
 
 render()
